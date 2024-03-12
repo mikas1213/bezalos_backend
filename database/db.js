2181243e02c66669c29cc0b09c50cbc3fe386e4d
Mikas Dulevicius <mikas1213@gmail.com>
2024-03-12 19:39:09 +0200

    fixing gitignore files

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: 'mikas',
-    host: 'localhost',
-    database: 'bezalos',
-    post: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = {
