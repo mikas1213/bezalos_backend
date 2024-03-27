f5c1749b1bc1ec1f2db0de79d120513db70bd57d
Mikas Dulevicius <mikas1213@gmail.com>
2024-03-27 21:11:41 +0200

    fix cors origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://www.dulevicius.dev',
     'https://bezalos.dulevicius.dev',
+    'https://www.bezalos.dulevicius.dev',
     'http://127.0.0.1:5173',
     'http://localhost:5173'
 ];
