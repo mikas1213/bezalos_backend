c70b08bb5e95c7fb2184a068d1115b022037f414
Mikas Dulevicius <mikas1213@gmail.com>
2024-04-20 13:37:10 +0300

    making dynamic templates

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,30 @@
 const sgMail = require('@sendgrid/mail');
 
 const messages = {
-    profilis: 'Jungiantis į bendruomenę pasirinkai, kad tave domina sveikas svorio metimas, šį tikslą lengviausiai tau padės pasiekti narystė "MINI"'
+    profilis: {
+        text: 'Jungiantis į bendruomenę pasirinkai, kad tave domina sveikas svorio metimas, šį tikslą lengviausiai tau padės pasiekti narystė "MINI"', 
+        btn: 'Narystė'
+    },
+    virtuve: {
+        text: 'Jungiantis į bendruomenę pasirinkai, kad tave domina išmokti sveikatai palankios mitybos pagrindų, šį tikslą lengviausiai tau padės pasiekti narystė Valgau be žalos | Virtuvėje',
+        btn: 'Į Virtuvę'
+    },
+    abu: {
+        text: 'Jungiantis į bendruomenę pasirinkai, kad tave domina išmokti sveikatai palankios mitybos pagrindų bei sveikai sumažinti savo kūno svorį, šį tikslą lengviausiai tau padės pasiekti narystė "MAXI"',
+        bth: 'Narystė'        
+    },
+    nezinau: {
+        text: 'Jungiantis į bendruomenę pasirinkai, kad dar nežinai kas tave šiame projekte domina, tad kviečiu pasižvalgyti',
+        btn: 'Pasižvalgyti'
+    }
 }
 
 module.exports = class Email {
     constructor(user, initial_target, token) {
-        console.log('==========================================\n');
-        console.log('email: ', user.email);
-        console.log('initial_target:', initial_target);
-        console.log('token: ', token);
+        // console.log('==========================================\n');
+        // console.log('email: ', user.email);
+        // console.log('initial_target:', initial_target);
+        // console.log('token: ', token);
 
         this.email = user.email;
         this.initial_target = initial_target;
@@ -20,12 +35,17 @@ module.exports = class Email {
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
         const message = {
-            from: { email: process.env.SENDGRID_EMAIL_FROM },
+            from: { 
+                email: process.env.SENDGRID_EMAIL_FROM,
+                name: "Sandra | Valgau be žalos",
+            },
             personalizations: [{ 
                 to: [{ email: this.email }],
                 dynamic_template_data: {
                     "token": this.token,
-                    "message": messages[this.initial_target]
+                    "subject": "Tavo registracija sėkminga 🥳",
+                    "message": messages[this.initial_target].text,
+                    "button": messages[this.initial_target].btn
                 }
             }],
             template_id: template
@@ -34,11 +54,11 @@ module.exports = class Email {
     }
 
     async welcome() {
-        console.log('SENDGRID_WELCOME_TEMPLATE_ID: ', process.env.SENDGRID_WELCOME_TEMPLATE_ID);
-        console.log('SENDGRID_RESET_PSW_TEMPLATE_ID: ', process.env.SENDGRID_RESET_PSW_TEMPLATE_ID);
-        console.log('check: ', 'd-87d1d111a55d4f3597d8cca5f7b9b406');
+        // console.log('SENDGRID_WELCOME_TEMPLATE_ID: ', process.env.SENDGRID_WELCOME_TEMPLATE_ID);
+        // console.log('SENDGRID_RESET_PSW_TEMPLATE_ID: ', process.env.SENDGRID_RESET_PSW_TEMPLATE_ID);
+        // console.log('check: ', 'd-87d1d111a55d4f3597d8cca5f7b9b406');
         // await this.sendEmail(process.env.SENDGRID_WELCOME_TEMPLATE_ID);
-        // await this.sendEmail('d-87d1d111a55d4f3597d8cca5f7b9b406');
+        await this.sendEmail('d-87d1d111a55d4f3597d8cca5f7b9b406');
     }
 
     async resetPassword() {
