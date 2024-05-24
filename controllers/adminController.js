14493bc140bd5f1890054a9d46bca4980d3da406
Mikas Dulevicius <mikas1213@gmail.com>
2024-05-24 19:45:12 +0300

    fix file path

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,7 @@ const db = require('../database/db');
 
 exports.getUsers = async (req, res, next) => {
     try {
-        const data = await db.query('SELECT * from users');
+        const data = await db.query('SELECT * from users ORDER BY subscription_expires ASC;');
         res.status(200).json({
             users: data.rows
         });
