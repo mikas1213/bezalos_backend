640d9623dc8e6ec94815f27c4f6daeaeda4b9fb7
Mikas Dulevicius <mikas1213@gmail.com>
2024-06-07 14:43:09 +0300

    change secret KEY

--- utils/payments.js
+++ utils/payments.js
@@ -12,12 +12,9 @@ exports.stripeSession = async (user_id, user_email, priceId, plan_name) => {
                 price: priceId,
                 quantity: 1
             }],
-            success_url: 'https://bezalos.dulevicius.dev/paslaugos',
-            cancel_url: 'https://bezalos.dulevicius.dev'
-            
-            // success_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:5173/apmoketa-sekmingai?session_id={CHECKOUT_SESSION_ID}' : '/apmoketa-sekmingai?session_id={CHECKOUT_SESSION_ID}'}`,
-            // cancel_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:5173/paslaugos' : '/paslaugos'}`,
-            // metadata: { user_id, subscription_status: plan_name }
+            success_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:5173/apmoketa-sekmingai?session_id={CHECKOUT_SESSION_ID}' : 'https://bezalos.dulevicius.dev/paslaugos/apmoketa-sekmingai?session_id={CHECKOUT_SESSION_ID}'}`,
+            cancel_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:5173/paslaugos' : 'https://bezalos.dulevicius.dev/paslaugos'}`,
+            metadata: { user_id, subscription_status: plan_name }
         });
         console.log('Session: ', session)
         return session;
