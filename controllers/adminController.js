cb4e5003ea6f126a0590f668acc9bd80f72d2d90
Mikas Dulevicius <mikas1213@gmail.com>
2024-08-24 12:41:10 +0300

    add filter to admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,46 +4,39 @@ const path = require('node:path');
 
 const { trys_lentos }  = require('../utils/sqlQueries');
 
-// exports.getAllUsers_old = async (req, res) => {
-    
-    // const { column = 'subscription_expires', sort = 'ASC'} = req.query;
-    // const columns = 'id, name, email, role, subscription, subscription_type, initial_target, subscription_expires, assigned_plan, nutrition_tracking, nutrition_plan_status, support_over, last_activity';
-    // let queryString = `SELECT ${columns} from users where role = $1 ORDER BY ${column} ${sort} NULLS LAST;`;
-    // let queryParams = [2324];
-
-    // if(column === 'subscription_type') {
-    //     queryString = `SELECT ${columns} from users where role = $1 AND ${column} = $2;`;
-    //     queryParams = [2324, 'free'];
-    // }
-
-
-    // const columns = 'users.id, name, email, role, subscription, subscription_type, initial_target, subscription_expires, assigned_plan, nutrition_tracking, nutrition_plan_status, support_over, last_activity, subscriptions.status as s_status, subscriptions.current_period_end as s_subscription_expires';
-    // let queryString = `SELECT ${columns} from users LEFT JOIN subscriptions ON users.id = subscriptions.user_id where role = $1 ORDER BY ${column} ${sort} NULLS LAST;`;
-    // let queryParams = [2324];
-
-    // if(column === 'subscription_type') {
-    //     queryString = `SELECT ${columns} from users LEFT JOIN subscriptions ON users.id = subscriptions.user_id where role = $1 AND ${column} = $2;`;
-    //     queryParams = [2324, 'free'];
-    // }
-
-    // try {
-    //     const data = await db.query(queryString, queryParams);
-        
-    //     res.status(200).json({
-    //         users: data.rows
-    //     });
-    // } catch (err) {
-    //     console.log(err.message);
-    // }
-// };
-
 exports.getAllUsers = async (req, res) => {
-    const { column, sort } = req.body;
-    try {
-        const columns = 'users.id, name, email, role, subscription, subscription_type, initial_target, subscription_expires, assigned_plan, nutrition_tracking, nutrition_plan_status, support_over, last_activity, subscriptions.status as s_status, subscriptions.current_period_end as s_subscription_expires';
-        let queryString = `SELECT ${columns} from users LEFT JOIN subscriptions ON users.id = subscriptions.user_id where role = $1 ORDER BY ${column} ${sort} NULLS LAST;`;
+    
+    const { column, sort, week, month } = req.body;
+    
+    const columns = 'users.id, name, email, role, subscription, subscription_type, initial_target, subscription_expires, assigned_plan, nutrition_tracking, nutrition_plan_status, support_over, last_activity, subscriptions.status as s_status, subscriptions.current_period_end as s_subscription_expires';
+    let where = 'where role = $1';
+    let queryParams = [2324];
 
-        const data = await db.query(queryString, [2324]);        
+    var from = new Date();
+    var to = new Date();
+
+    if(week && month) {
+        from.setDate(from.getDate() - 14);
+        queryParams[1] = from.toLocaleString('lt-LT');
+        where = `where role = $1 AND assigned_plan < $2`;
+    } else if(week) {
+        from.setDate(from.getDate() - 28);
+        to.setDate(to.getDate() - 14);
+        queryParams[1] = from.toLocaleString('lt-LT');
+        queryParams[2] = to.toLocaleString('lt-LT');
+        where = `where role = $1 AND assigned_plan BETWEEN $2 AND $3`;
+
+    } else if(month) {
+        from.setDate(from.getDate() - 28);
+        queryParams[1] = from.toLocaleString('lt-LT');
+        where = `where role = $1 AND assigned_plan < $2`;
+    }
+        
+    let queryString = `SELECT ${columns} from users LEFT JOIN subscriptions ON users.id = subscriptions.user_id ${where} ORDER BY ${column} ${sort} NULLS LAST;`;
+
+    try {
+        console.log(queryParams)
+        const data = await db.query(queryString, queryParams);        
         res.status(200).json({
             users: data.rows
         });
@@ -54,7 +47,7 @@ exports.getAllUsers = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
     try {
-
+        console.log(req.body)
         let { column, value } = req.body;
         if(value === '') value = null;
         let queryString = `UPDATE users SET ${column} = $1 WHERE id = $2;`;
