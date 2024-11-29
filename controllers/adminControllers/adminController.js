1a39663d6b19bbecdd09fd1a3dc7d6886530ccd7
Mikas Dulevicius <mikas1213@gmail.com>
2024-11-29 15:18:09 +0200

    Add stats to mitybos planai navbar

--- controllers/adminControllers/adminController.js
+++ controllers/adminControllers/adminController.js
@@ -20,13 +20,16 @@ exports.getStats = async (req, res) => {
             (SELECT COUNT(*) FROM mailer_list) AS mailer_list_mails,
             (SELECT COUNT(*) FROM offers) AS offer_mails,
             (SELECT count(*) from users WHERE subscription_type = 'Virtuvė' AND role = 2324) AS virtuve_active,
-            (SELECT count(*) from users WHERE subscription_type = 'Profilis' AND role = 2324) AS profilis_active;
+            (SELECT count(*) from users WHERE subscription_type = 'Profilis' AND role = 2324) AS profilis_active,
+            (SELECT count(*) from food_plans) AS plans,
+            (SELECT count(*) from food_meals) AS meals,
+            (SELECT count(*) from food_products) AS products;
         `);
-
-        res.status(200).json({
-            data: data.rows[0]
-        });
+        
+        res.status(200).json(data.rows[0]);
     } catch (err) {
-        console.log(err.message)
+        res.status(500).json({
+            message: err.message
+        });
     }
 };
