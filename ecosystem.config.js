adc2a6183eed7b7091174004131cd8e265e3eba2
Mikas Dulevicius <mikas1213@gmail.com>
2024-12-01 10:59:24 +0200

    add ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [{
       name: "Be žalos",
       script: "./server.js",
-      error_file: "pm2_logs/error.log",    // Make sure these paths exist
-      out_file: "pm2_logs/out.log",
-      log_file: "pm2_logs/combined.log"
+      error_file: "logs_pm2/error.log",    // Make sure these paths exist
+      out_file: "logs_pm2/out.log",
+      log_file: "logs_pm2/combined.log"
     }]
   }
