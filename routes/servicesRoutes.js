cce2628c944f15d84aad07dac0ffd2324b72c013
Mikas Dulevicius <mikas1213@gmail.com>
2025-01-03 15:19:58 +0200

    fix customers router

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const servicesController = require('../controllers/servicesController');
 
-router.route('/').get(
-    servicesController.getAllServices
-);
-
 router.route('/:slug').get(
     servicesController.getOneService
+);
+
+router.route('/').get(
+    servicesController.getAllServices
 );
 
 module.exports = router;
