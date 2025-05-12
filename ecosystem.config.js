4e8ec04ba68cb8bed0971d8d0c9958c9e206302c
Mikas <mikas1213@gmail.com>
2025-05-12 12:35:06 +0300

    fix ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,14 @@ module.exports = {
         merge_logs: true,
         time: true,
         autorestart: true,
-        // watch: true,
+        watch: false,
         time: true,
         // ignore_watch: ["logs"],
         instances: "max",
-        exec_mode: "cluster"
+        exec_mode: "cluster",
+        kill_timeout: 5000,       // Duoti 5s procesui užbaigti darbą prieš SIGKILL
+        wait_ready: true,         // Laukti, kol procesas nusiųs 'ready' signalą
+        listen_timeout: 10000,    // Laukti 10s, kol procesas pradės klausytis
+        max_memory_restart: "1G"
     }]
   }
