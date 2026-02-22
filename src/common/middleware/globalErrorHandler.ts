844097bb97f3c592477ac40e89dbcd87507f1308
Mikas <mikas1213@gmail.com>
2026-02-22 08:33:36 +0200

    refactor: global error handling

--- src/common/middleware/globalErrorHandler.ts
+++ src/common/middleware/globalErrorHandler.ts
@@ -2,50 +2,52 @@ import { Request, Response, NextFunction } from 'express';
 import { logEvents } from './logger';
 
 interface AppError extends Error {
-    statusCode?: number;
-    status?: 'fail' | 'error';
-    isOperational?: boolean;
+	statusCode?: number;
+	status?: 'fail' | 'error';
+	isOperational?: boolean;
+	errors?: Record<string, string[]>;
 }
 
 const sendErrorDev = (err: AppError, res: Response): Response => {
-    console.error('ERROR 💥', err);
-    
-    return res.status(err.statusCode ?? 500).json({
-        status: err.status ?? 'error',
-        error: err,
-        message: err.message,
-        stack: err.stack
-    });
+	console.error('ERROR 💥', err);
+
+	return res.status(err.statusCode ?? 500).json({
+		status: err.status ?? 'error',
+		error: err,
+		message: err.message,
+		stack: err.stack,
+	});
 };
 
 const sendErrorProd = (err: AppError, res: Response): Response => {
-    if (err.isOperational) {
-        return res.status(err.statusCode ?? 500).json({
-            status: err.status,
-            message: err.message
-        });
-    }
-    
-    console.error('ERROR 💥', err);
-    return res.status(500).json({
-        status: 'error',
-        message: 'Something went wrong!'
-    });
+	if (err.isOperational) {
+		return res.status(err.statusCode ?? 500).json({
+			status: err.status,
+			message: err.message,
+			...(err.errors && { errors: err.errors }),
+		});
+	}
+
+	console.error('ERROR 💥', err);
+	return res.status(500).json({
+		status: 'error',
+		message: 'Something went wrong!',
+	});
 };
 
 export const globalErrorHandler = (
-    err: AppError, 
-    req: Request, 
-    res: Response, 
-    next: NextFunction
+	err: AppError,
+	req: Request,
+	res: Response,
+	next: NextFunction,
 ) => {
-    err.statusCode = err.statusCode ?? 500;
-    err.status = err.status ?? 'error';
-    logEvents(`${err.name}: ${err.message}`, 'errors_log.txt');
-    
-    if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
-    } else {
-        sendErrorProd(err, res);
-    }
+	err.statusCode = err.statusCode ?? 500;
+	err.status = err.status ?? 'error';
+	logEvents(`${err.name}: ${err.message}`, 'errors_log.txt');
+
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	} else {
+		sendErrorProd(err, res);
+	}
 };
